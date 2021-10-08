cb0e08e08855f433c7331f3eac2eea721643ef32
Ryan10937 <76846047+Ryan10937@users.noreply.github.com>
2021-10-08 14:25:51 -0500

    Added File explorer

--- project1_front_end.py
+++ project1_front_end.py
@@ -4,17 +4,11 @@
 
 # %%
 
-#cd OneDrive\SchoolFiles\CSCE\Graduate\"CSCE 5214 - Software for AI"
-
-
 import streamlit as st
 import sys
-#import cnn_model
-#from tkinter import Tk     # from tkinter import Tk for Python 3.x
-#import tkinter 
-#import askopenfilename
 import easygui
 import wx
+#import cnn_model
 
 def get_path(wildcard):
     app = wx.App(None)
@@ -29,14 +23,11 @@ def get_path(wildcard):
 
 
 def run_model(filename) -> bool:
-    #place model function call here
+    # TODO: place model function call here
     return True
 
 def load_file():
 
-    #filename = tkinter.filedialog.askopenfilename() # show an "Open" dialog box and return the path to the selected file
-    #filename = tkinter.filedialog.open() # show an "Open" dialog box and return the path to the selected file
-    #filename = easygui.fileopenbox(default='OneDrive\SchoolFiles\CSCE\Graduate\"CSCE 5214 - Software for AI"')
     filename = get_path("(*.txt)|*.txt")
     
     try:
@@ -54,12 +45,9 @@ def load_file():
         st.error('File not found.')
 
 def main():
-    #Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
     st.title("MRI Brain Tumor Detection")
     st.write("An app made in **Python** with **Jupyter**")
-    #file_path = st.text_input('Enter file path to MRI Scans:')
     st.button("Open File Explorer", on_click = load_file)
-    #streamlit.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
 
 if __name__ == '__main__':
     main()
