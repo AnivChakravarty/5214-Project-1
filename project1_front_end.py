e086c8651029ae5468b78fb3c5a61b57842ec6e1
Ryan10937 <76846047+Ryan10937@users.noreply.github.com>
2021-10-08 14:24:24 -0500

    Add files via upload

--- project1_front_end.py
+++ project1_front_end.py
@@ -4,41 +4,66 @@
 
 # %%
 
+#cd OneDrive\SchoolFiles\CSCE\Graduate\"CSCE 5214 - Software for AI"
+
 
 import streamlit as st
 import sys
 #import cnn_model
+#from tkinter import Tk     # from tkinter import Tk for Python 3.x
+#import tkinter 
+#import askopenfilename
+import easygui
+import wx
 
-def run_model(file_path) -> bool:
+def get_path(wildcard):
+    app = wx.App(None)
+    style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
+    dialog = wx.FileDialog(None, 'Open', wildcard=wildcard, style=style)
+    if dialog.ShowModal() == wx.ID_OK:
+        path = dialog.GetPath()
+    else:
+        path = None
+    dialog.Destroy()
+    return path
+
+
+def run_model(filename) -> bool:
     #place model function call here
     return True
 
-def load_file(file_path):
+def load_file():
+
+    #filename = tkinter.filedialog.askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    #filename = tkinter.filedialog.open() # show an "Open" dialog box and return the path to the selected file
+    #filename = easygui.fileopenbox(default='OneDrive\SchoolFiles\CSCE\Graduate\"CSCE 5214 - Software for AI"')
+    filename = get_path("(*.txt)|*.txt")
+    
     try:
-        with open(file_path) as input:
-            st.text(input.read())
+        with open(filename) as input:
+            #st.text(input.read())
             st.text("File opened. Running model")
-            has_tumor = run_model(file_path)
+
+            has_tumor = run_model(filename)
             if has_tumor:
                 st.text("Tumor Positive")
             else:
                 st.text("Tumor Negative")
-                
-                
+
     except FileNotFoundError:
         st.error('File not found.')
 
-st.title("MRI Brain Tumor Detection",anchor="test")
-st.write("An app made in **Python** with **Jupyter**")
-file_path = st.text_input('Enter file path to MRI Scans:')
-st.button("Submit File", on_click = load_file(file_path))
-#streamlit.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
+def main():
+    #Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    st.title("MRI Brain Tumor Detection")
+    st.write("An app made in **Python** with **Jupyter**")
+    #file_path = st.text_input('Enter file path to MRI Scans:')
+    st.button("Open File Explorer", on_click = load_file)
+    #streamlit.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
 
-
+if __name__ == '__main__':
+    main()
 
 
 # %%
-
-
-
 
